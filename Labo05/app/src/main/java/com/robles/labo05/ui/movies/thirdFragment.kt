8adab5a303fcfa1a473b7a3d66fd2f457e6bd62f
KevinC911 <00157320@uca.edu.sv>
2023-05-06 22:09:03 -0600

    finishing the labo

--- Labo05/app/src/main/java/com/robles/labo05/ui/movies/thirdFragment.kt
+++ Labo05/app/src/main/java/com/robles/labo05/ui/movies/thirdFragment.kt
@@ -19,11 +19,6 @@ import com.robles.labo05.databinding.FragmentThirdBinding
 class thirdFragment : Fragment() {
 
     private val viewModel: MovieViewModel by activityViewModels{ MovieViewModel.Factory }
-    private lateinit var nameInput: EditText
-    private lateinit var categoryInput: EditText
-    private lateinit var descriptionInput: EditText
-    private lateinit var qualificationInput: EditText
-    private lateinit var buttonAddMovie: Button
     private lateinit var binding: FragmentThirdBinding
 
     override fun onCreateView(
@@ -36,28 +31,8 @@ class thirdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bind(view)
-        listeners()
-    }
-
-    private fun listeners(){
-        buttonAddMovie.setOnClickListener{
-            viewModel.addMovies(
-                MovieModel(nameInput.text.toString(),
-            categoryInput.text.toString(),
-            descriptionInput.text.toString(),
-            qualificationInput.text.toString())
-            )
-        }
-        Log.d("movies", viewModel.getMovies().toString())
-    }
-
-    private fun bind(view: View) {
-        nameInput = view.findViewById(R.id.name_input)
-        categoryInput = view.findViewById(R.id.category_input)
-        descriptionInput = view.findViewById(R.id.description_input)
-        qualificationInput = view.findViewById(R.id.calification_input)
-        buttonAddMovie = view.findViewById(R.id.add_movie)
+        setViewModel()
+        observeStatus()
     }
 
     private fun setViewModel() {
@@ -81,6 +56,10 @@ class thirdFragment : Fragment() {
 
             }
         }
+
+    companion object {
+        const val APP_TAG = "APP_TAG"
+    }
 
 
 
